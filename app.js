8ed7792b7c75813d006ef82a61711d1aed4af020
Andy Herzog <asherzog19@gmail.com>
2017-02-04 17:57:36 -0700

    explored gif and bakcgorund image

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 $(() => {
-  new Vivus("logo", {type: 'oneByOne', duration: 200, file:'./assets/logo5.svg'}, finished);
+  let logo = new Vivus("logo", {type: 'oneByOne', duration: 200, file:'./assets/logo6.svg'}, finished);
 
   function finished(obj) {
-    obj.el.classList.add('finished');
+    obj.parentEl.classList.add('finished');
+    console.log(obj);
   }
 });
