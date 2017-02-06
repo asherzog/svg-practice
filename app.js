a747183c23a4605b68d3362151f65ae9a4afe6d3
Andy Herzog <asherzog19@gmail.com>
2017-02-06 14:18:10 -0700

    :carousel_horse: carousel added

--- app.js
+++ app.js
@@ -1,6 +1,30 @@
 $(() => {
   let logo = new Vivus("logo", {type: 'oneByOne', duration: 200, file:'./assets/logo6.svg'}, finished);
 
+  var flky = new Flickity( '.pics', {
+    accessibility: true,
+    adaptiveHeight: false,
+    autoPlay: 5000,
+    cellAlign: 'center',
+    cellSelector: undefined,
+    contain: false,
+    draggable: true,
+    dragThreshold: 3,
+    freeScroll: false,
+    friction: 0.2,
+    groupCells: false,
+    initialIndex: 0,
+    lazyLoad: false,
+    percentPosition: true,
+    prevNextButtons: true,
+    pageDots: true,
+    resize: true,
+    rightToLeft: false,
+    setGallerySize: true,
+    watchCSS: false,
+    wrapAround: true
+  });
+
 });
 
 function finished(vivus) {
